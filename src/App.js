d4107d516536d1e583a5415c2db97717d49fedd9
Michael Joseph Gelvez <gelvezmichael@yahoo.com>
2021-10-19 12:57:04 +0800

    [FIRST COMMIT]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Add from './pages/Add';
+import Edit from './pages/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-mono">
+          <h1 class="mb-12 text-5xl">SIMPLE CALENDAR APP</h1>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/add" component={Add} />
+            <Route exact path="/edit/:id" component={Edit} />
+        </Switch>        
     </div>
   );
 }
